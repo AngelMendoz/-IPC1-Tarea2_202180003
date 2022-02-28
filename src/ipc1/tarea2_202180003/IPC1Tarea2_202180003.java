b2d8a99d59e89a20ceff5c0057e81e79f2147c53
AngelMendoz <angelisaias27112000@gmail.com>
2022-02-27 20:06:49 -0600

    Login-1.0

---
+++ src/ipc1/tarea2_202180003/IPC1Tarea2_202180003.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ipc1.tarea2_202180003;
+
+
+public class IPC1Tarea2_202180003 {
+          
+   
+    public static void main(String[] args) {
+       LoginTarea lolt = new LoginTarea();
+       lolt.setVisible(true);
+       //dispose(true);
+    }
+    
+}
